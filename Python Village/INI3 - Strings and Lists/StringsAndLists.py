81011ec55a5576eb3916677d8f932a01bf67a8a7
schin7 <schin7@gmail.com>
2018-01-15 11:26:53 -0500

    Added Python Exercises

---
+++ Python Village/INI3 - Strings and Lists/StringsAndLists.py
@@ -0,0 +1,6 @@
+s = "opahgPr3tX6T4lkANJC7dMzANnzyz7cRz7kBBUMnREQvJ4ZEcNTG5gDN4ButeoFw0Kl9Etwn2DjbXhcHqu3lXiw2NnZsNCOo9JsJEBuoEifZfXbWKKzyKVkKZY5czerskiikKSzqA4JwyIi4eVTrOI8tacHSKjcswlavkFtL2tJejhYTSjHJDnMP9qA3i9fZ."
+a = 57
+b = 61
+c = 123
+d = 130
+print s[a:b+1] + " " + s[c:d+1]
---
+++ Python Village/INI4 - Conditions and Loops/ConditionsAndLoops.py
@@ -0,0 +1,8 @@
+a = 4375
+b = 8455
+sum = 0
+for i in range(a,b+1):
+    if i%2 == 1:
+        sum += i
+print sum
+
